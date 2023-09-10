042142b1d3e05a95f67452d0e513489159c21cf3
minhquang1102 <64059264+minhquang1102@users.noreply.github.com>
2023-09-11 06:17:07 +0700

    upload product & news

---
+++ product/js/index.js
@@ -0,0 +1,74 @@
+// scroll fullpage
+new fullpage('#fullpage', {
+    //options here
+    autoScrolling: true,
+    scrollHorizontally: true,
+    navigation: true,
+    navigationPosition: 'right',
+});
+
+// swiper slider page1
+var swiper = new Swiper(".mySwiper", {
+    cssMode: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+});
+
+// click tabs
+var tabLinks = document.querySelectorAll(".tablinks");
+var tabContent = document.querySelectorAll(".page2__tabItem");
+
+tabLinks.forEach(function(el) {
+    el.addEventListener("click", openTabs);
+});
+
+
+function openTabs(el) {
+    var btn = el.currentTarget; // lắng nghe sự kiện và hiển thị các element
+    var stone = btn.dataset.stone; // lấy giá trị trong data-stone
+
+    tabContent.forEach(function(el) {
+        el.classList.remove("active");
+    }); //lặp qua các tab content để remove class active
+
+    tabLinks.forEach(function(el) {
+        el.classList.remove("active");
+    }); //lặp qua các tab links để remove class active
+
+    document.querySelector("#" + stone).classList.add("active");
+    // trả về phần tử đầu tiên có id="" được add class active
+
+    btn.classList.add("active");
+    // các button mà chúng ta click vào sẽ được add class active
+}
+
+// swiper slider page3
+var swiper = new Swiper(".mySlider", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        820: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 20,
+        },
+    },
+});
